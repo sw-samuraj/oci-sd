7ca42ccd67c6a8124f0a19094e9c2c4bfcad202a
Vít Kotačka <vit.kotacka@oracle.com>
2018-09-07 15:27:24 +0200

    Remove go1.10.x from Travis CI build

--- config_test.go
+++ config_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestReadConfigFile(t *testing.T) {
-	configFile = "conf/oci-sd.toml"
+	configFile = "testdata/oci-sd.toml"
 	cf, _ := readConfigFile()
 
 	if len(cf) == 0 {
